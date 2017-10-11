247928d9aa3316278432b4b7ef788ffa9668b717
zaqwsx55 <zaqwsx@poczta.fm>
2017-10-11 12:16:55 +0200

    Utworzenie klasy Baby

---
+++ src/app/babies/babies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-babies',
+  templateUrl: './babies.component.html',
+  styleUrls: ['./babies.component.scss']
+})
+export class BabiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
