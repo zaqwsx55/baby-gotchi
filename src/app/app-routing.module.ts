fa0c3d276f0102d5c9adf4354d8f5ec73d122748
zaqwsx55 <zaqwsx@poczta.fm>
2017-10-16 21:33:01 +0200

    Poprawienie routingu i BabyStatusComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
       path: '',
       component: BabiesComponent
     }, {
-      path: ':id',
+      path: ':id/care',
       component: BabyComponent,
       pathMatch: 'prefix'
     }]
