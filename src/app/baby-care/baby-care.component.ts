ebce30009933908930b27e485a518234a462782d
zaqwsx55 <zaqwsx@poczta.fm>
2017-10-17 09:10:47 +0200

    Edycja BabyCareComponent

--- src/app/baby-care/baby-care.component.ts
+++ src/app/baby-care/baby-care.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FirebaseApp } from 'angularfire2';
 
 @Component({
   selector: 'app-baby-care',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BabyCareComponent implements OnInit {
 
-  constructor() { }
+  babyId;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private firebaseApp: FirebaseApp) { }
 
   ngOnInit() {
+    this.activatedRoute.parent.params.subscribe(
+      p => this.babyId = p['id']
+    );
+  }
+
+  feedBaby() {
+    const statRef = this.firebaseApp.database().ref(`/babies/${this.babyId}/hunger`);
+    statRef.transaction(stat => stat - 10 < 0 ? 0 : stat - 10);
+  }
+
+  cleanBaby() {
+    const statRef = this.firebaseApp.database().ref(`/babies/${this.babyId}/shittiness`);
+    statRef.transaction(stat => stat - 10 < 0 ? 0 : stat - 10);
+  }
+
+  sleepBaby() {
+    const statRef = this.firebaseApp.database().ref(`/babies/${this.babyId}/sleepiness`);
+    statRef.transaction(stat => stat - 10 < 0 ? 0 : stat - 10);
+  }
+
+  cuddleBaby() {
+    const statRef = this.firebaseApp.database().ref(`/babies/${this.babyId}`);
+    statRef.transaction(baby => {
+      if (baby) {
+        baby.hunger = this.getDecreasedStat({statGetter: () => baby.hunger, modifier: 10});
+        baby.shittiness = this.getDecreasedStat({statGetter: () => baby.shittiness, modifier: 10});
+        baby.sleepiness = this.getDecreasedStat({statGetter: () => baby.sleepiness, modifier: 10});
+        baby.life = this.getIncreasedStat({statGetter: () => baby.life, modifier: 1});
+      }
+      return baby;
+    });
+  }
+
+  getDecreasedStat({statGetter, modifier}) {
+    return statGetter() > modifier ? statGetter() - modifier : 0;
+  }
+
+  getIncreasedStat({statGetter, modifier}) {
+    return statGetter() + modifier > 100 ? 100 : statGetter() + modifier;
   }
 
 }
