af365ffc6842b7262ed8739997ed2d5573b38b50
zaqwsx55 <zaqwsx@poczta.fm>
2017-10-18 12:21:37 +0200

    Wdrożenie na hostingu Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+
+  production: true,
+
+  // Firebase configuration
+  firebase: {
+    apiKey: 'AIzaSyDFL76YOusDceZYYRuEpjmtyNE3UmV4Ulc',
+    authDomain: 'baby-gotchi-dbca4.firebaseapp.com',
+    databaseURL: 'https://baby-gotchi-dbca4.firebaseio.com',
+    projectId: 'baby-gotchi-dbca4',
+    storageBucket: '',
+    messagingSenderId: '475056967084'
+  }
+
 };
